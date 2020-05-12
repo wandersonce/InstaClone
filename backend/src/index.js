5ae559b8bc61946c0f00b07b43e119b132c2c9a4
Wanderson Castro <wandersonce1@hotmail.com>
2020-05-11 21:12:51 -0700

    Mongoose and db connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://wanderson:100492@twittercloneapp-frhue.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.get('/', (req, res) => {
+    return res.send('hello world!!')
+})
+
+app.listen(3333);
