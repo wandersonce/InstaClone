bc0f3946446c3043733210ca2910083dab5d4d14
Wanderson Castro <wandersonce1@hotmail.com>
2020-05-12 21:21:52 -0700

    Backend finished
    Backend finished with socket.io and cors added

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -14,11 +14,14 @@ module.exports = {
         const { author, place, description, hashtags } = req.body;
         const { filename: image } = req.file;
 
+        const [name] = image.split('.');
+        const filename = `${name}.jpg`
+
         await sharp(req.file.path)
             .resize(500)
             .jpeg({ quality: 70 })
             .toFile(
-                path.resolve(req.file.destination, 'resized', image)
+                path.resolve(req.file.destination, 'resized', filename)
             )
 
         fs.unlinkSync(req.file.path);
@@ -28,8 +31,10 @@ module.exports = {
             place,
             description,
             hashtags,
-            image,
+            image: filename,
         })
+
+        req.io.emit('post', post);
 
         return res.json(post);
     }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect('mongodb+srv://wanderson:100492@twittercloneapp-frhue.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+app.use((req, res, next) => {
+    req.io = io;
+
+    next();
+})
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+
 app.use(require('./routes'));
 
-app.listen(3333);
+server.listen(3333);
