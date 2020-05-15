71a0822847054f2d2d039293301b1d12fe201044
Wanderson Castro <wandersonce1@hotmail.com>
2020-05-14 22:55:51 -0700

    Feed style done

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Header.css';
 
@@ -9,8 +10,12 @@ export default function Header() {
     return (
         <header id="main-header">
             <div className="header-content">
-                <img src={logo} alt="InstaClone" />
-                <img src={camera} alt="Send Post" />
+                <Link to="/">
+                    <img src={logo} alt="InstaClone" />
+                </Link>
+                <Link to="/new">
+                    <img src={camera} alt="Send Post" />
+                </Link>
             </div>
         </header>
     );
--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,10 +1,72 @@
 import React, { Component } from 'react';
 
+import './Feed.css'
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
 class Feed extends Component {
     render() {
         return (
-            <h1>Feed!</h1>
-        )
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Wanderson Castro</span>
+                            <span className="place">VANCOUVER</span>
+                        </div>
+
+                        <img src={more} alt="More" />
+                    </header>
+
+                    <img src="http://localhost:3333/files/Feed-100.jpg" alt="" />
+
+                    <footer >
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+
+                            <strong>900 Likes</strong>
+
+                            <p>
+                                Amazing first post from react!
+                                <span>#react #omnistack</span>
+                            </p>
+                        </div>
+                    </footer>
+                </article>
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Wanderson Castro</span>
+                            <span className="place">VANCOUVER</span>
+                        </div>
+
+                        <img src={more} alt="More" />
+                    </header>
+
+                    <img src="http://localhost:3333/files/Feed-100.jpg" alt="" />
+
+                    <footer >
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+
+                            <strong>900 Likes</strong>
+
+                            <p>
+                                Amazing first post from react!
+                                <span>#react #omnistack</span>
+                            </p>
+                        </div>
+                    </footer>
+                </article>
+            </section>
+        );
     }
 }
 
