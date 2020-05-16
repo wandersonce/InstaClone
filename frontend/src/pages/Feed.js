df7898f0a2bd8b105267aa20ef27b6bf2fe1845b
Wanderson Castro <wandersonce1@hotmail.com>
2020-05-15 23:16:11 -0700

    Like and Post function Real time
    Using socket.io to

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../services/api';
+import io from 'socket.io-client';
 
 import './Feed.css'
 
@@ -14,9 +15,31 @@ class Feed extends Component {
     };
 
     async componentDidMount() {
+        this.registerToSocket();
+
         const response = await api.get('posts');
 
         this.setState({ feed: response.data })
+    };
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333');
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] });
+        })
+
+        socket.on('like', likePost => {
+            this.setState({
+                feed: this.state.feed.map(post =>
+                    post._id === likePost._id ? likePost : post
+                )
+            })
+        })
+    };
+
+    handleLike = id => {
+        api.post(`/posts/${id}/like`)
     };
 
     render() {
@@ -38,9 +61,15 @@ class Feed extends Component {
 
                         <footer >
                             <div className="actions">
-                                <img src={like} alt="" />
-                                <img src={comment} alt="" />
-                                <img src={send} alt="" />
+                                <button type="button" onClick={() => this.handleLike(post._id)}>
+                                    <img src={like} alt="" />
+                                </button >
+                                <button type="button">
+                                    <img src={comment} alt="" />
+                                </button>
+                                <button type="button">
+                                    <img src={send} alt="" />
+                                </button>
 
                                 <strong>{post.likes} likes</strong>
 
