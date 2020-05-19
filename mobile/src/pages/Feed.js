722987f0bd94c34ca687498d09944b03ac7dcb5b
Wanderson Castro <wandersonce1@hotmail.com>
2020-05-18 22:05:00 -0700

    Header and routes done

--- mobile/src/pages/Feed.js
+++ mobile/src/pages/Feed.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image, TouchableOpacity } from 'react-native'
+
+import camera from '../assets/camera.png';
 
 export default class Feed extends Component {
+    static navigationOptions = {
+        headerRight: (
+            <TouchableOpacity style={{ marginRight: 20 }} onPress={() => { }}>
+                <Image source={camera} />
+            </TouchableOpacity>
+        ),
+    };
     render() {
         return (
             <View>
-                <Text> textInComponent </Text>
+                <Text> Feed </Text>
             </View>
         )
     }
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,13 +1,23 @@
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack';
-
+import React from 'react';
+import { Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack'; {/* CreateStack navigator that will create the top header and will work layers */ }
 
 import Feed from './pages/Feed';
 import New from './pages/New';
+
+import logo from './assets/logo.png';
 
 export default createAppContainer(
     createStackNavigator({
         Feed,
         New
+    }, {
+        defaultNavigationOptions: {
+            headerTintColor: '#000',
+            headerTitle: <Image style={{ marginHorizontal: 20 }} source={logo} />,
+            headerBackTitle: null,
+        },
+        mode: 'modal'
     })
 );
